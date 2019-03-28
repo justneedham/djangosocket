7fe6969bc6299c671e5337e24538582bae2480eb
Justin Needham <justin.needham@alumnimail.pepperdine.edu>
2019-03-28 09:22:53 -0600

    add channels dev server

--- DjangoSocket/settings.py
+++ DjangoSocket/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app.apps.AppConfig',
+    'channels'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Channels
+ASGI_APPLICATION = 'DjangoSocket.routing.application'
